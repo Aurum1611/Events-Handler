e8d84134164f79564c39627edba6c93101e962a8
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2020-01-24 12:41:44 +0530

    Second commit: Made basic layout for RegisterActivity and added email validation code

--- app/src/main/java/com/internship/project/eventshandler/RegisterActivity.java
+++ app/src/main/java/com/internship/project/eventshandler/RegisterActivity.java
@@ -1,8 +1,11 @@
 package com.internship.project.eventshandler;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.internship.project.eventshandler.util.EmailValidator;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        findViewById(R.id.register).setOnClickListener(v -> {
+            String firstName = ((TextView) findViewById(R.id.first_name)).getText().toString();
+            String lastName = ((TextView) findViewById(R.id.last_name)).getText().toString();
+            String email = ((TextView) findViewById(R.id.email_address)).getText().toString();
+
+            if (!EmailValidator.isEmailValid(email))
+                return;
+
+            // TODO: ADD FirebaseDatabase registration code here
+
+        });
     }
+
 }
